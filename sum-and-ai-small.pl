b99e82106a6c6e955a37a518c9fa205de1e01714
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2021-04-20 14:42:35 -0400

    Adds various files

---
+++ sum-and-ai-small.pl
@@ -0,0 +1,10 @@
+my $sum = 0;
+while($line = <>) {
+  if($line =~ /(\S+\s+\d+\:\s+)(\d+)\s+seqs/) { 
+    my ($month, $count) = ($1, $2);
+    $sum += $count;
+    printf("%s%d seqs (%10.4f pt); %10d total (%10.4f pt);\n",
+           $month, $count, $count / 20, $sum, $sum / 20);
+  }
+  else { die "ERROR couldn't parse line $line"; }
+}
